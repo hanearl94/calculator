0bdf79278c7a74c286c218147366d874568daf60
hanearl94 <144743870+hanearl94@users.noreply.github.com>
2025-07-16 12:53:42 -0700

    feat: Implement simple calculator with error handling and loop

--- calculator.py
+++ calculator.py
@@ -1,1 +1,34 @@
-print("Simple Calculator (+,-,*, /, %")
+from unittest import result
+
+print("============ Simple Calculator (+, -, *, /, %) ============")
+
+while True:
+    try:
+        num1 = float(input("Enter the first number: "))
+
+        op = input("Enter operation (+, -, *, /, %): ")
+
+        num2 = float(input("Enter the second number: "))
+
+        if op == "+":
+            result == num1 + num2
+        elif op == "-":
+            result == num1 - num2
+        elif op == "*":
+            result == num1 * num2
+        elif op == "/":
+            if num2 != 0:
+                result == num1 / num2
+            else:
+                result = "Error: Cannot divide by Zero"
+        elif op == "%":
+            if num2 != 0:
+                result == num1 % num2
+            else:
+                result = "Error: Cannot modulo by Zero"
+        else:
+            result = "Error: Invalid operation."
+    except ValueError:
+        result = "Error: Invalid number."
+
+    print("Result: ", result)
