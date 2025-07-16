0156498f640d9a58899e84f7798a392d29d06425
hanearl94 <144743870+hanearl94@users.noreply.github.com>
2025-07-16 12:59:36 -0700

    feat: adding option to continue or quit + fixing error with = and ==

--- calculator.py
+++ calculator.py
@@ -11,19 +11,19 @@ while True:
         num2 = float(input("Enter the second number: "))
 
         if op == "+":
-            result == num1 + num2
+            result = num1 + num2
         elif op == "-":
-            result == num1 - num2
+            result = num1 - num2
         elif op == "*":
-            result == num1 * num2
+            result = num1 * num2
         elif op == "/":
             if num2 != 0:
-                result == num1 / num2
+                result = num1 / num2
             else:
                 result = "Error: Cannot divide by Zero"
         elif op == "%":
             if num2 != 0:
-                result == num1 % num2
+                result = num1 % num2
             else:
                 result = "Error: Cannot modulo by Zero"
         else:
@@ -32,3 +32,8 @@ while True:
         result = "Error: Invalid number."
 
     print("Result: ", result)
+
+    count = input("Do you want to continue? (y/n): ")
+    if count.lower() != "y":
+        print("Calculator exited. Goodbye!")
+        break
